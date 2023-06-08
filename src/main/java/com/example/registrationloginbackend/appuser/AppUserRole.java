196d12d82a18f3b169167504a4708ad5e9e73602
Misa <rudy-2000w@mail.ru>
2023-06-08 19:06:07 +0600

    first commit

---
+++ src/main/java/com/example/registrationloginbackend/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.registrationloginbackend.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/example/registrationloginbackend/RegistrationLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
